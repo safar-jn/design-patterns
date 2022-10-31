6f5a6f925aad105df8a1bd80119346a107266d73
Jan Šafář <safarj13@fit.cvut.cz>
2022-10-31 20:19:55 +0100

    test(ghb): add dummy CPP files

---
+++ pattern1/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by John on 31/10/2022.
+//
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	std::cout << "Hello world!" << std::endl;
+	return 0;
+}
---
+++ pattern2/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by John on 31/10/2022.
+//
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	std::cout << "Hello world2!" << std::endl;
+	return 0;
+}
