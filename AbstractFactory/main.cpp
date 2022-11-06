7e3bd529b6b839f2998410abf51a9001e6aaba4f
safar-jn <safarj13@cvut.cz>
2022-11-06 16:09:22 +0100

    feat(abstract-factory): init structure

---
+++ AbstractFactory/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "src/Foo.h"
+
+int main(int argc, char **argv)
+{
+	// TODO: demonstrate usage of design pattern
+	std::cout << Foo::hello_world() << std::endl;
+	return 1;
+}
---
+++ AbstractFactory/src/Foo.h
@@ -0,0 +1,14 @@
+#ifndef DESIGN_PATTERNS_FOO_H
+#define DESIGN_PATTERNS_FOO_H
+
+
+#include <string>
+
+class Foo
+{
+	public:
+		static std::string hello_world();
+};
+
+
+#endif //DESIGN_PATTERNS_FOO_H
