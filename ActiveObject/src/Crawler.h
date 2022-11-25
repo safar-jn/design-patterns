0b4ec5be527fe31f4faa881c30a8f67dc4fd7b7f
safar-jn <safarj13@cvut.cz>
2022-11-25 19:25:22 +0100

    feat(active-object): add example sourcecode

---
+++ ActiveObject/src/Crawler.h
@@ -0,0 +1,29 @@
+#ifndef DESIGN_PATTERNS_CRAWLER_H
+#define DESIGN_PATTERNS_CRAWLER_H
+
+
+#include "ThreadSafeQueue.h"
+#include "ThreadSafeQueue.cpp"
+
+#include <string>
+#include <thread>
+#include <iostream>
+
+
+/// active object -> starts its own internal thread which executes registered tasks
+class Crawler
+{
+    public:
+             Crawler   ();
+        void terminate ();
+        void crawl     (const std::string &website);
+    private:
+        void executeTask ();
+
+        ThreadSafeQueue<std::string> _tasks;
+        std::thread                  _worker;
+        bool                         _run;
+};
+
+
+#endif //DESIGN_PATTERNS_CRAWLER_H
