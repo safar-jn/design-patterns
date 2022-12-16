3309afe78b6f36d9ad5d76271607da02381c95cc
safar-jn <safarj13@cvut.cz>
2022-12-16 16:56:05 +0100

    feat(master-slave): refactor

--- MasterSlave/main.cpp
+++ MasterSlave/main.cpp
@@ -3,19 +3,28 @@
 #include <iostream>
 
 
-int main (int argc, char **argv)
+void simulateClient ()
 {
-    /// easy to work with once set up
-    /// - we just request something from the master and don't care about the implementation details
-    /// - but under the hood the core creates multiple concurrently running crawlers (i.e. slaves)
+    std::cout << "[main] | trigger scraping process for [john, doe]" << std::endl;
+
+    // ---
+
+    // easy to work with once set up
+    // - we just request something from the master (ScraperCore) and don't care about the implementation details
+    // - but under the hood the core creates multiple concurrently running slaves (Crawlers)
     ScraperCore core;
     auto res = core.scrapeClient("john", "doe");
 
     // ---
 
-    std::cout << "scraping for [john,doe]" << std::endl;
+    std::cout << "[main] | results of scraping for [john,doe]" << std::endl;
     for (const auto &r: res)
-        std::cout << "\t" << r << std::endl;
+        std::cout << " |- " << r << std::endl;
+    std::cout << std::endl;
+}
 
+int main (int argc, char **argv)
+{
+    simulateClient(); // simulate usage (only communicating with master)
     return 0;
 }
